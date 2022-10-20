437415fad85f3b5a245ff36c67f643622113d3ef
horsellcommon <crawfordj716@gmail.com>
2022-10-20 11:20:13 +0100

    Added functions for User table

---
+++ src/movie/userFunctions.js
@@ -0,0 +1,29 @@
+const User = require("./userTable.js");
+
+exports.createUser = async (userObject) => {
+  try {
+    await User.create(userObject);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.readUser = async (filterObject) => {
+  try {
+    if (filterObject) {
+      return await User.findOne({ where: filterObject });
+    } else {
+      return await User.findAll();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.deleteUser = async (deleteObject) => {
+  try {
+    await User.destroy({ where: deleteObject });
+  } catch (error) {
+    console.log(error);
+  }
+};
